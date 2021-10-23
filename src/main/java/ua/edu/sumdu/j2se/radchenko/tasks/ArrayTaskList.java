cc120bb80dc2b1595cc24793fefdaf4b511e561a
FaPaH <fapah2000@gmail.com>
2021-10-23 17:24:51 +0300

    added exeptions in method add,getTask and changed all remarks what been in commit #2

--- src/main/java/ua/edu/sumdu/j2se/radchenko/tasks/ArrayTaskList.java
+++ src/main/java/ua/edu/sumdu/j2se/radchenko/tasks/ArrayTaskList.java
@@ -4,26 +4,31 @@ import java.util.Arrays;
 
 public class ArrayTaskList {
 
-    private Task[] arrayOfTasks = new Task[5];
+    private int size = 0;
+    private Task[] arrayOfTasks = new Task[10];
 
-    public void add(Task task){
-        arrayOfTasks = Arrays.copyOf(arrayOfTasks, arrayOfTasks.length + 1);
+    public void add(Task task) throws IllegalArgumentException{
+        size++;
+        arrayOfTasks = Arrays.copyOf(arrayOfTasks, arrayOfTasks.length + size);
         if(task != null){
-            for (int i = 0; i < arrayOfTasks.length ;i++){
+            for (int i = 0; i < size ;i++){
                 if (arrayOfTasks[i] == null) {
                     arrayOfTasks[i] = task;
                     i++;
                 }
             }
         }
+        else{
+            throw new IllegalArgumentException("Task cant be null");
+        }
     }
 
     public boolean remove(Task task){
         if (task != null) {
-            for (int i = 0; i < arrayOfTasks.length; i++) {
+            for (int i = 0; i < size; i++) {
                 if (arrayOfTasks[i].equals(task)) {
                     System.arraycopy(arrayOfTasks, i + 1, arrayOfTasks, i, arrayOfTasks.length - 1 - i);
-                    arrayOfTasks = Arrays.copyOf(arrayOfTasks, arrayOfTasks.length - 1);
+                    size--;
                     return true;
                 }
             }
@@ -32,33 +37,38 @@ public class ArrayTaskList {
     }
 
     public int size(){
-        return arrayOfTasks.length;
+        return size;
     }
 
-    public Task getTask(int index){
-        if (index > arrayOfTasks.length){
-            System.out.println("index out of bounds");
+    public Task getTask(int index) throws IndexOutOfBoundsException{
+        if (index > size){
+            throw new IndexOutOfBoundsException("index cant be more than: " + index);
         }
         return arrayOfTasks[index];
     }
 
     public ArrayTaskList incoming(int from, int to) {
         ArrayTaskList arrayTaskList = new ArrayTaskList();
-        for (int i = 0; i < arrayOfTasks.length; i++) {
-            if (arrayOfTasks[i].isActive()) {
-                if (arrayOfTasks[i].getStartTime() > from && arrayOfTasks[i].getEndTime() < to
-                        || arrayOfTasks[i].getTime() > from && arrayOfTasks[i].getTime() < to) {
-                    arrayTaskList.add(arrayOfTasks[i]);
+        for (int i = 0; i < size; i++) {
+            Task task = arrayOfTasks[i];
+            if (task.isActive()) {
+                if (task.nextTimeAfter(task.getStartTime()) > from
+                        && task.nextTimeAfter(task.getEndTime()) < to
+                        && task.getTime() < to) {
+                        arrayTaskList.add(arrayOfTasks[i]);
                 }
+
+                // I think this variant what if commented is better
+
+//            if (arrayOfTasks[i].isActive()) {
+//                if (arrayOfTasks[i].getStartTime() > from && arrayOfTasks[i].getEndTime() < to
+//                        || arrayOfTasks[i].getTime() > from && arrayOfTasks[i].getTime() < to) {
+//                    arrayTaskList.add(arrayOfTasks[i]);
+//                }
+//            }
+
             }
         }
         return arrayTaskList;
-    }
-
-    @Override
-    public String toString() {
-        return "ArrayTaskList{" +
-                "arrayOfTasks=" + Arrays.toString(arrayOfTasks) +
-                '}';
     }
 }
